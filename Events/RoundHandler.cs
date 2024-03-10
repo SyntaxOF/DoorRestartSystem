31a5966b5e890a0d1e1674518cd760ffc65a6847
boyda <boyda@DAVID-CODING>
2024-03-10 21:12:15 +0100

    Projektdateien hinzufügen.

---
+++ Events/RoundHandler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoorRestartSystem.Events
+{
+    internal sealed class RoundHandler
+    {
+
+    }
+}
